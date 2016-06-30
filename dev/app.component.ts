3414a8c214129a7dffab4ab40891c38ce1af5b6a
Kadee1344 <kd@1212c0081.local>
2016-06-30 11:38:02 +0300

    Attribute Directive Part1. Highlight. @Directive, ngOnInit, Renderer.

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,29 +1,13 @@
 import {Component} from '@angular/core';
-import {InputComponent} from './bindings/input.component';
-import {ConfirmComponent} from './bindings/confirm.component';
-
+import {AttributeDirectives} from './attribute-directives.component';
 
 @Component({
     selector: 'my-app',
     template: `
-      <div class="container">
-        <my-input (submitted)="onSubmit($event)" [myself]="confirmedMyself"></my-input>
-      </div>
-      <div class="container">
-        <my-confirm (confirmed)="onConfirm($event)" [myself]="myself"></my-confirm>
-      </div>
+      <my-attribute-directives></my-attribute-directives>
     `,
-    directives: [InputComponent, ConfirmComponent]
+    directives: [AttributeDirectives]
 })
 export class AppComponent {
-  myself = {name: '', age: ''};
-  confirmedMyself = {name: '', age: ''};
 
-  onSubmit(myself: {name: string, age: string}){
-    this.myself = {name: myself.name, age: myself.age};
-  }
-
-  onConfirm(myself: {name: string, age: string}){
-    this.confirmedMyself = {name: myself.name, age: myself.age};
-  }
 }
