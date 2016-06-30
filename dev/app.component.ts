0b8e4145ef07bdd4807c91eef0b2ed57e7f18d94
Kadee1344 <kd@1212c0081.local>
2016-06-30 13:54:30 +0300

    StructuralDirectives. Own directives. (ngFor, tgif, switch ..).

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,12 +1,14 @@
 import {Component} from '@angular/core';
 import {AttributeDirectives} from './attribute-directives.component';
+import {StructuralDirectives} from './structural-directives.component';
 
 @Component({
     selector: 'my-app',
     template: `
       <my-attribute-directives></my-attribute-directives>
+      <my-structural-directives></my-structural-directives>
     `,
-    directives: [AttributeDirectives]
+    directives: [AttributeDirectives, StructuralDirectives]
 })
 export class AppComponent {
 
